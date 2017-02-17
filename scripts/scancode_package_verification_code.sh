7fe62f3b16d634a6ce245e45fe431303aec05238
Sebastian Schuberth <sschuberth@gmail.com>
2017-02-17 09:28:49 +0100

    Add a script that calculates the package verification code for ScanCode

---
+++ scripts/scancode_package_verification_code.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ ! -f "$1" ]; then
+    echo "Please pass an existing ScanCode JSON file as the argument."
+    exit 1
+fi
+
+if ! which jq > /dev/null 2>&1; then
+    echo "This script depends on https://stedolan.github.io/jq/."
+    echo "Please put the 'jq' executable into your PATH."
+    exit 2
+fi
+
+jq -r .files[].sha1 "$1" | sort | tr -d '\n' | sha1sum - | cut -d' ' -f1
